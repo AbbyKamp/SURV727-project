21d5d0090f2d6b1f1a8dccb311481f846c1ea99d
AbbyKamp <abby.kamp@gmail.com>
2019-10-28 16:42:45 -0400

    washington state data cleaning
    Cleaned washington state results data

--- SURV727-final-paper-data-cleaning.R
+++ SURV727-final-paper-data-cleaning.R
@@ -1,7 +1,15 @@
 #SURV727 Final Project
 
-#uploading washington-state data
-results <- read.csv("~/final-paper/washington-2018-results.csv")
+#packages to install
+library(dplyr)
 
-#download twitter data
-#data visualization ggplot
+#uploading washington-state data
+wash_data <- read_csv("~/Data-cleaning/final-paper/data/washington-2018-results.csv")
+head(wash_data)
+
+#filtering results to only keep intiative 1631
+results <-
+wash_data %>%
+  select(County, Race, Candidate, Votes, PercentageOfTotalVotes) %>%
+  filter(Race == "State Measures Initiative Measure No. 1639 Initiative Measure No. 1639 concerns firearms.")
+
