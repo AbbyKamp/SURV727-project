68df23151855b5951e9c8153e4656c0ba76fa09d
[name] <[rameesha@terpmail.umd.edu]>
2019-10-30 17:46:55 -0400

    Update SURV727-final-paper-data-cleaning.R

--- SURV727-final-paper-data-cleaning.R
+++ SURV727-final-paper-data-cleaning.R
@@ -12,4 +12,14 @@ results <-
 wash_data %>%
   select(County, Race, Candidate, Votes, PercentageOfTotalVotes) %>%
   filter(Race == "State Measures Initiative Measure No. 1639 Initiative Measure No. 1639 concerns firearms.")
+  
+# Twitter API
+create_token(
+  app = "Project727",
+  consumer_key = "9QNMbZyUIoD0PhIX8ucEqqILp",
+  consumer_secret = "qBoORiu2VgDJAXHqceq24XJzmLyfq5ktwXXAI06n98uhQ3HJCk",
+  access_token = "1188871434740862976-JhfoMtGIvOs0S7WcnLov0A5VgzlUle",
+  access_secret = "VriQtJZRAeWi2x8m6WrZoEAxpCWn5ROoNqInujL4M379d"
+)
+
 
